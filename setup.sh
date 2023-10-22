73d7ba41483b075f3fb8e8b8fc5b044c29aa9522
Jack Parsons <jackpar1113@gmail.com>
2023-10-21 20:00:11 -0600

    update setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.config ~/.config
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s .vimrc ~/.vimrc
+ln -s .zshrc ~/.zshrc
+ln -s .config ~/.config
+ln -s .bashrc ~/.bashrc
+ln -s .gitconfig ~/.gitconfig
+ln -s .ideavimrc ~/.ideavimrc
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .oh-my-zsh ~/.oh-my-zsh
+ln -s .bash_profile ~/.bash_profile
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 os_name=$(uname -s)
 if [ "$os_name" == "Darwin" ]; then
