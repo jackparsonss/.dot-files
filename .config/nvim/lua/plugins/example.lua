eed6a1c93fdecf6e98179042dbd0cd942e2106c0
Jack Parsons <jparsons@ualberta.ca>
2025-02-18 12:28:48 -0700

    new config

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,46 +1,25 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
 return {
-  -- add pyright to lspconfig
   {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
     opts = {
-      ---@type lspconfig.options
-      servers = {
-        clangd = {
-          cmd = { "clangd", "--background-index" }, -- Specify the command and any additional flags
-          filetypes = { "c", "cpp" }, -- Set the filetypes for which the LSP server should be enabled
-          root_dir = function(fname) -- Specify how to find the project root directory
-            return require("lspconfig").util.root_pattern("compile_commands.json")(fname)
-              or require("lspconfig").util.root_pattern("CMakeLists.txt", "compile_flags.txt", ".git")(fname)
-              or require("lspconfig").util.root_pattern("compile_flags.txt")(fname)
-          end,
-          settings = {
-            clangd = {
-              path = "/usr/bin/clangd", -- Path to the clangd executable
-              cmd = { "clangd", "--background-index" }, -- Additional clangd command-line options
-              useBinaryCache = true, -- Enable/disable binary cache for clangd
-              capabilities = {}, -- Additional capabilities
-              fallbackFlags = {}, -- Additional flags to use when the compiler flags cannot be determined
-              filetypes = { "c", "cpp" }, -- Set the filetypes for which clangd will be enabled
-              completeUnimported = true, -- Complete unimported headers
-              semanticHighlighting = true, -- Enable semantic highlighting
-            },
-          },
-        },
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
       },
     },
   },
 
-  -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -50,15 +29,10 @@ return {
         "javascript",
         "json",
         "lua",
-        "c",
-        "cpp",
-        "markdown",
-        "markdown_inline",
-        "python",
         "query",
+        "regex",
         "tsx",
         "typescript",
-        "vim",
         "yaml",
       },
     },
@@ -66,55 +40,4 @@ return {
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
-     -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  -- then: setup supertab in cmp
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
-  },
 }
